6cb133f6a9c146a14a64cf7cfdd4b27af0d5c654
Ilja <iljazaicev6@gmail.com>
2025-01-16 11:03:55 +0200

    Part 1

--- Calcalutor.py
+++ Calcalutor.py
@@ -1,2 +1,14 @@
 print ("This is calculator")
 
+print("Welcome to the Calculator!")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("Select an operation:")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+
+operation = input("Enter the number of the operation (1/2/3/4): ")
